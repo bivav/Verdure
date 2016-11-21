c351bf6950eb06d4c5a8ed574913a05d733a823a
Bivav <bivavraj@gmail.com>
2016-11-21 22:19:04 +0530

    First Commit

--- app/src/main/java/com/cyberChautari/verdure/DetailsRadish.java
+++ app/src/main/java/com/cyberChautari/verdure/DetailsRadish.java
@@ -3,9 +3,7 @@ package com.cyberChautari.verdure;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-/**
- * Created by Bishruti on 1/15/2015.
- */
+
 public class DetailsRadish extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/cyberChautari/verdure/Display_Button_Layout.java
@@ -0,0 +1,9 @@
+package com.cyberChautari.verdure;
+
+import android.support.v7.app.ActionBarActivity;
+
+public class Display_Button_Layout extends ActionBarActivity {
+
+
+
+}
